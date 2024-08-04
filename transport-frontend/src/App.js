19fdffa4e770028659cd433389d0904e5914bbba
polalco13 <103862173+polalco13@users.noreply.github.com>
2024-08-04 22:56:53 +0200

    Refactor CSS styles for improved readability and maintainability

--- transport-frontend/src/App.js
+++ transport-frontend/src/App.js
@@ -1,20 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
 import React, { useState, useEffect } from 'react';
-import rutasData from './data/rutas.json';
 import rutasAgostData from './data/rutasAgost.json';
-import horariosData from './data/horarios.json';
 import horariosAgostData from './data/horariosAgost.json';
-import Maintenance from './manteniment';
-
 import './styles.css';
 
 function App() {
-
-  //provisional
-  const [isMaintenance, setIsMaintenance] = useState(true); // Estado para el modo de mantenimiento
-
-  const [comentarios, setComentarios] = useState({});
-
   const [origen, setOrigen] = useState('');
   const [destino, setDestino] = useState('');
   const [resultados, setResultados] = useState([]);
@@ -24,17 +14,11 @@ function App() {
   const [horariosCompletos, setHorariosCompletos] = useState([]);
   const [mostrarHorarios, setMostrarHorarios] = useState(false);
   const [mensajeNoMasBuses, setMensajeNoMasBuses] = useState('');
-  const [mostrarPopup, setMostrarPopup] = useState(false);
   const [rutaPredeterminada, setRutaPredeterminada] = useState('');
-  const [favoritos, setFavoritos] = useState([]);
-  const [mostrarFavoritos, setMostrarFavoritos] = useState(false);
   const [loading, setLoading] = useState(false);
-
-
   const diasSemana = ['Diumenge', 'Dilluns', 'Dimarts', 'Dimecres', 'Dijous', 'Divendres', 'Dissabte'];
 
   useEffect(() => {
-    //canviar
     setRutas(rutasAgostData);
   }, []);
 
@@ -54,10 +38,6 @@ function App() {
   }, [rutaPredeterminada]);
 
   useEffect(() => {
-    localStorage.setItem('favoritos', JSON.stringify(favoritos));
-  }, [favoritos]);
-
-  useEffect(() => {
     if (origen && destino) {
       buscarProximoBus(origen, destino);
       if (mostrarHorarios) {
@@ -68,12 +48,12 @@ function App() {
       setHorariosCompletos([]);
       setMostrarHorarios(false);
       setMensajeNoMasBuses('');
-      } 
+    }
   }, [origen, destino, diaSemana, mostrarHorarios]);
 
   const buscarProximoBus = (origenBus, destinoBus) => {
     const ara = new Date();
-    const horaActual = ara.toTimeString().split(' ')[0].slice(0, 5); // "HH:MM"
+    const horaActual = ara.toTimeString().split(' ')[0].slice(0, 5);
     const diaActual = diasSemana[ara.getDay()];
 
     const rutaIds = rutas
@@ -82,7 +62,6 @@ function App() {
 
     let resultados = [];
 
-    //canviar mes!
     horariosAgostData.forEach(horario => {
       if (rutaIds.includes(horario.ruta_id) && horario.horarios[diaActual]) {
         const proximosHorarios = horario.horarios[diaActual].filter(hora_salida => {
@@ -124,14 +103,8 @@ function App() {
 
     const proximosBuses = resultados.slice(0, 3);
 
-    console.log('Próximos buses encontrados:', proximosBuses);
-
     setResultados(proximosBuses);
-    if (proximosBuses.length === 0) {
-      setMensajeNoMasBuses('No hi ha més autobusos per avui');
-    } else {
-      setMensajeNoMasBuses('');
-    }
+    setMensajeNoMasBuses(proximosBuses.length === 0 ? 'No hi ha més autobusos per avui' : '');
   };
 
   const buscarHorariosCompletos = (origenBus, destinoBus) => {
@@ -141,7 +114,6 @@ function App() {
 
     let resultados = [];
 
-    //canviar mes!
     horariosAgostData.forEach(horario => {
       if (rutaIds.includes(horario.ruta_id) && horario.horarios[diaSemana]) {
         horario.horarios[diaSemana].forEach(hora_salida => {
@@ -172,96 +144,49 @@ function App() {
   const detectarUbicacion = () => {
     setLoading(true);
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
-            let distanciaMinima = Infinity;
-            let origenCercano = '';
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        let distanciaMinima = Infinity;
+        let origenCercano = '';
 
-            rutasAgostData.forEach(ruta => {
-                const distancia = Math.sqrt(
-                    Math.pow(ruta.latitud_origen - latitude, 2) + Math.pow(ruta.longitud_origen - longitude, 2)
-                );
-                if (distancia < distanciaMinima) {
-                    distanciaMinima = distancia;
-                    origenCercano = ruta.origen;}
-            });
-            setOrigen(origenCercano); 
-            setLoading(false);
-          }, () => {
-            setLoading(false);
-          });
-        } else {
-            setLoading(false);
-        }  
-      };
-
-  const agregarAFavoritos = () => {
-    if (origen && destino) {
-      const nuevaRutaFavorita = { origen, destino };
-      if (!favoritos.some(fav => fav.origen === origen && fav.destino === destino)) {
-        setFavoritos([...favoritos, nuevaRutaFavorita]);
-      }
+        rutasAgostData.forEach(ruta => {
+          const distancia = Math.sqrt(
+            Math.pow(ruta.latitud_origen - latitude, 2) + Math.pow(ruta.longitud_origen - longitude, 2)
+          );
+          if (distancia < distanciaMinima) {
+            distanciaMinima = distancia;
+            origenCercano = ruta.origen;
+          }
+        });
+        setOrigen(origenCercano);
+        setLoading(false);
+      }, () => {
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
     }
   };
 
-  const eliminarFavorito = (favorito) => {
-    const nuevosFavoritos = favoritos.filter(fav => !(fav.origen === favorito.origen && fav.destino === favorito.destino));
-    setFavoritos(nuevosFavoritos);
-  };
-
-  const seleccionarFavorito = (favorito) => {
-    setOrigen(favorito.origen);
-    setDestino(favorito.destino);
-    setMostrarFavoritos(false);
-  };
-
-  const [showNotification, setShowNotification] = useState(true);
-
-  const closeNotification = () => {
-    setShowNotification(false);
-  };
-
-  const agregarComentario = (rutaId, comentario) => {
-    setComentarios(prevComentarios => {
-      const nuevosComentarios = { ...prevComentarios };
-      if (!nuevosComentarios[rutaId]) {
-        nuevosComentarios[rutaId] = [];
-      }
-      nuevosComentarios[rutaId].push(comentario);
-      return nuevosComentarios;
-    });
-  };
-
-
-    if (!isMaintenance) {
-      return <Maintenance />; // Renderiza el componente de mantenimiento si está en modo de mantenimiento
-    }
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Transport Públic del Penedès</h1>
-          {showNotification && (
-            <div className="notification">
-              <p>horaris d'Agost actualitzats.</p>
-              <button className="close-button" onClick={closeNotification}>❌</button>
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Transport Públic del Penedès</h1>
+        <div className="form-group">
+          <div className="form-fields">
+            <div className="form-field">
+              <label>Origen:</label>
+              <select value={origen} onChange={e => setOrigen(e.target.value)}>
+                <option value="">Selecciona</option>
+                {[...new Set(rutas.map(ruta => ruta.origen))].sort().map((origen, index) => (
+                  <option key={index} value={origen}>{origen}</option>
+                ))}
+              </select>
+              <button onClick={detectarUbicacion} disabled={loading}>
+                Detectar ubicació propera
+              </button>
+              {loading && <div className="spinner"></div>}
             </div>
-          )}
-          <div className="form-group">
-            <div className="form-fields">
-              <div className="form-field">
-                <label>Origen:</label>
-                <select value={origen} onChange={e => setOrigen(e.target.value)}>
-                  <option value="">Selecciona</option>
-                  {[...new Set(rutas.map(ruta => ruta.origen))].sort().map((origen, index) => (
-                    <option key={index} value={origen}>{origen}</option>
-                  ))}
-                </select>
-                <button onClick={detectarUbicacion} disabled={loading}>
-                    Detectar ubicació propera
-                  </button>
-                  {loading && <div className="spinner"></div>}
-                </div>
             <div className="form-field">
               <label>Destí:</label>
               <select value={destino} onChange={e => setDestino(e.target.value)} disabled={!origen}>
@@ -273,16 +198,9 @@ function App() {
             </div>
           </div>
           <button className="swap-button" onClick={intercambiarOrigenDestino}>⇄</button>
-          
-          {/*<img src={require('./img/VilaMCristina.png')} alt="Your Image" className="responsive-image"/>*/}
-
         </div>
         <button onClick={() => buscarProximoBus(origen, destino)}>Buscar següents busos</button>
         {mensajeNoMasBuses && <p>{mensajeNoMasBuses}</p>}
-
-        
-
-        {/* Renderización de resultados */}
         {resultados.length > 0 && (
           <div>
             <h2>Pròxims busos</h2>
@@ -295,7 +213,6 @@ function App() {
             </ul>
           </div>
         )}
-        
         <div>
           <label>Dia de la setmana:</label>
           <select value={diaSemana} onChange={e => setDiaSemana(e.target.value)}>
@@ -316,7 +233,7 @@ function App() {
               </li>
             ))}
           </ul>
-        )}        
+        )}
       </div>
       <Analytics />
     </div>
